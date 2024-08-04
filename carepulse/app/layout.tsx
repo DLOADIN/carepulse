4c109037d6ef5285da2e423acfd2b71a21b2489d
Manzi David <manzidavid73@gmail.com>
2024-08-04 03:19:11 +0200

    HEALTH APPLICATION

--- carepulse/app/layout.tsx
+++ carepulse/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/theme-provider";
+
 import { cn } from '@/lib/utils'
 
 const Mainfont = Plus_Jakarta_Sans({ 
@@ -21,8 +23,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en">
-      <body className={cn('min-h-screen bg-black font-sans antialiased', Mainfont.variable)}>{children}</body>
+      <body className={cn('min-h-screen bg-black font-sans antialiased', Mainfont.variable)}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+          >
+        {children}
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
+
