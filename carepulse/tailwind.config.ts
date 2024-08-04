78aec2bd3d4379ceea212fbd58e9968b3f266846
Manzi David <manzidavid73@gmail.com>
2024-08-04 02:18:48 +0200

    push

--- carepulse/tailwind.config.ts
+++ carepulse/tailwind.config.ts
@@ -68,11 +68,6 @@ const config = {
           "20%,50%": { opacity: "0" },
         },
       },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "caret-blink": "caret-blink 1.25s ease-out infinite",
-      },
     },
   },
   plugins: [require("tailwindcss-animate")],
