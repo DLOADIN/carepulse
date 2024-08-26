1baf28c42426c113614122bb1d139c1fcc093898
DLOADIN <m.david@alustudent.com>
2024-08-26 22:37:10 +0200

    HEALTH APPLICATION

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import PatientForm from "@/components/forms/PatientForm";
 import Link from "next/link";
 import Image from "next/image";
+import PassKeyModal from "@/components/ui/PassKeyModal";
 
+export default function Home( {searchParams}: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true';
 
-export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      { isAdmin && <PassKeyModal />}
+
       <section className="remove-scrollbar container my-auto">
           <div className="sub-container max-w-[496px]">
             <Image
---
+++ components/ui/PassKeyModal.tsx
@@ -0,0 +1,78 @@
+'use-client'
+
+import React from 'react';
+import { useState } from 'react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./alert-dialog"
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+import Image from "next/image";
+import { useRouter } from 'next/router';
+
+const PassKeyModal = () => {
+  const router = useRouter;
+  const [open, setOpen] = useState(false);
+
+const closeModal = () =>{
+  setOpen(false)
+}
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      {/* <AlertDialogTrigger>Open</AlertDialogTrigger> */}
+      <AlertDialogContent className="shad-alert-dialog">
+    <AlertDialogHeader>
+      <AlertDialogTitle className="flex items-start justify-between">
+        Admin Access Verification
+        <Image 
+          src="/assets/icons/close.svg"
+          width={20}
+          height={20}
+          alt="close"
+          onClick={() => closeModal()}
+          className="cursor-pointer"
+        />
+      </AlertDialogTitle>
+      <AlertDialogDescription>
+        To access the admin page, Please Enter the passkey
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+
+    <div>
+      <InputOTP maxLength={6} value={passKey} onChange={(value) => setPasskey(value)}>
+        <InputOTPGroup className="shad-otp">
+          <InputOTPSlot className="shad-otp-slot" index={0} />
+          <InputOTPSlot className="shad-otp-slot" index={1} />
+          <InputOTPSlot className="shad-otp-slot" index={2} />
+          <InputOTPSlot className="shad-otp-slot" index={3} />
+          <InputOTPSlot className="shad-otp-slot" index={4} />
+          <InputOTPSlot className="shad-otp-slot" index={5} />
+        </InputOTPGroup>
+      </InputOTP>
+
+    </div>
+
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction>Continue</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
+
+  )
+}
+
+export default PassKeyModal
