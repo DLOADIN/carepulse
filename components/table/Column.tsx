aff7ee67864bedd285238d0828d28ba3a1ccefa8
DLOADIN <m.david@alustudent.com>
2024-08-28 21:07:08 +0200

    HEALTH APPLICATION

--- components/table/Column.tsx
+++ components/table/Column.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from "next/image";
 import { ColumnDef } from "@tanstack/react-table"
 import { MoreHorizontal } from "lucide-react"
 import {
@@ -11,6 +12,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "../button"
+import StatusBadge from "@/components/Status-badge"
+import { formatDateTime } from "@/lib/utils"
+import { Doctors } from "@/constants"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -24,24 +28,51 @@ export type Payment = {
 export const columns: ColumnDef<Payment>[] = [
   
   {
-    accessorKey: "status",
-    header: "Status",
+    header: "ID",
+    cell: ({ row} ) => <p className="text-14-medium">{row.index+1}</p>
   },
   {
-    accessorKey: "email",
-    header: "Email",
+    accessorKey: 'patient',
+    header: 'Patient',
+    cell: ({ row} ) =>{
+      return <p className="text-14-medium">{row.original.patient.name}</p>
+    }
   },
   {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
+    accessorKey: 'status',
+    header:'Status',
+    cell: ( { row }) =>{
+      <div className="min-w-[115px]">
+        <StatusBadge status={row.original.status}/>
+      </div>
+    }
+  },
+  {
+    accessorKey: "schedule",
+    header: "Appointment",
+    cell: ({ row }) => (
+      <p className="text-14-regular min-w-[100px]">
+        {formatDateTime(row.original.schedule).dateTime}
+      </p>
+    )
+  },
+  {
+    accessorKey: "primaryPhysician",
+    header: "Doctor",
     cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"))
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount)
- 
-      return <div className="text-right font-medium">{formatted}</div>
+      const doctor = Doctors.find((doc) => doc.name === row.original.primaryPhysician);
+
+      return(
+        <div className="flex items-center gap-3">
+          <Image 
+          src={ doctor?.image }
+          alt={doctor.name}
+          width={100}
+          height={100}
+          className=""
+          />
+        </div>
+      )
     },
   },
   {
