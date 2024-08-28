e59523fa3bd1386cd7633646861eeaa0e798b780
DLOADIN <m.david@alustudent.com>
2024-08-28 21:19:44 +0200

    HEALTH APPLICATION

--- components/table/Column.tsx
+++ components/table/Column.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { ColumnDef } from "@tanstack/react-table"
-import { MoreHorizontal } from "lucide-react"
+import AppointmentModal from "@/components/appointmentModal"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -66,40 +66,26 @@ export const columns: ColumnDef<Payment>[] = [
         <div className="flex items-center gap-3">
           <Image 
           src={ doctor?.image }
-          alt={doctor.name}
+          alt={ doctor.name}
           width={100}
           height={100}
-          className=""
+          className="size-8"
           />
+          <p className="whitespace-nowrap">
+            Dr. {doctor?.name}
+          </p>
         </div>
       )
     },
   },
   {
     id: "actions",
+    header: () => <div className="pl-4">Actions</div>,
     cell: ({ row }) => {
-      const payment = row.original
- 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+      return(
+        <div className="flex gap-1">
+          <AppointmentModal /> 
+        </div>
       )
     },
   },
